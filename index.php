590ad243a6fbf1bc449f504a714f9e3a8c1cd3e1
Silvia Nuñez <sinual@floridauniversitaria.es>
2025-01-07 18:08:31 +0100

    Añadidos archivos ganar.php y perder.php a index.php

--- index.php
+++ index.php
@@ -38,14 +38,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['letra'])) {
 
 // Comprobar si se ha ganado o perdido
 if ($_SESSION['letras_acertadas'] == $_SESSION['palabra']) {
-    echo "¡Enhorabuena! Has ganado :) La palabra era: " . $_SESSION['palabra'] . "<br>";
     session_destroy();
-    echo '<a href="">Jugar de nuevo</a>';
+    header("Location:ganar.php");
     exit();
 } elseif ($_SESSION['vidas'] <= 0) {
-    echo "Lo siento, has perdido :( La palabra era: " . $_SESSION['palabra'] . "<br>";
     session_destroy();
-    echo '<a href="">Jugar de nuevo</a>';
+    header("Location:perder.php");
     exit();
 }
 ?>
